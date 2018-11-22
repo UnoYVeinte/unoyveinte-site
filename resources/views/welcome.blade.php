f3043177b9816309610db483e9688f822759568e
Nks <niko.afv@gmail.com>
2018-11-22 10:55:01 -0300

    update title and favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <link rel="shortcut icon" type="image/png" href="/img/unoyveinte_small_32x32.png"/>
+        <link rel="shortcut icon" type="image/png" href="/img/unoyveinte_small_32x32.png"/>
+
+        <title>UnoYVeinte</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
